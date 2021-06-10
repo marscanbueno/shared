ab90a5d5dd49c5072eba72c55e555e4dddf7351e
marscanbueno <17656000+marscanbueno@users.noreply.github.com>
2021-06-10 00:55:46 -1000

    updates

--- .install.sh
+++ .install.sh
@@ -3,16 +3,18 @@ cd $HOME
 mkdir .dotfiles && cd .dotfiles
 
 # hosts
+sudo mv 
 curl -LJO https://raw.githubusercontent.com/marscanbueno/shared/main/hosts
+sudo cp hosts /etc/hosts
+
 
 # motd
 curl -LJO https://raw.githubusercontent.com/marscanbueno/shared/main/motd
+sudo cp motd /etc/motd
+
 
 # policybanner.rtfd
 
 mkdir policybanner.rtfd && cd policybanner.rtfd
 curl -LJO https://raw.githubusercontent.com/marscanbueno/shared/main/policybanner.rtfd/TXT.rtf
 curl -LJO https://raw.githubusercontent.com/marscanbueno/shared/main/policybanner.rtfd/Logo.png
-
-# exit
-exit
