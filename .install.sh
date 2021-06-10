a9b6cb7d05007414d4c309e5b20ca73a22e7f2fa
marscanbueno <17656000+marscanbueno@users.noreply.github.com>
2021-06-10 01:07:54 -1000

    Update .install.sh

--- .install.sh
+++ .install.sh
@@ -17,7 +17,7 @@ sudo chmod o+r /etc/motd
 
 mkdir policybanner.rtfd && cd policybanner.rtfd
 curl -LJO https://raw.githubusercontent.com/marscanbueno/shared/main/policybanner.rtfd/TXT.rtf
-curl -LJO https://raw.githubusercontent.com/marscanbueno/shared/main/policybanner.rtfd/Logo.png
+curl -LJO https://raw.githubusercontent.com/marscanbueno/shared/main/policybanner.rtfd/LOGO.png
 sudo cp -r policybanner.rtfd /Library/Security/PolicyBanner.rtfd
 sudo chmod -R o+rx /Library/Security/PolicyBanner.rtfd
 
