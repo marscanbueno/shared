7065c17ccda6373d46fb0976ede7e9f5d20dad2f
marscanbueno <17656000+marscanbueno@users.noreply.github.com>
2021-06-10 01:21:37 -1000

    Update .install.sh

--- .install.sh
+++ .install.sh
@@ -1,6 +1,7 @@
 
 # ask for password up-front.
 sudo -v
+
 # Keep-alive: update existing sudo time stamp if set, otherwise do nothing.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
@@ -27,6 +28,7 @@ sudo cp -r PolicyBanner.rtfd /Library/Security/PolicyBanner.rtfd
 sudo chmod -R o+rx /Library/Security/PolicyBanner.rtfd
 
 # Justincase, lol..  Just in case!
+
     # diskutil apfs updatePreboot /
 
 # Clean up..
