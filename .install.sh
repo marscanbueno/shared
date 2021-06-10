4c73eae3f2bc68aa7a3abb64b69fcdbe17862d2c
marscanbueno <17656000+marscanbueno@users.noreply.github.com>
2021-06-10 01:18:58 -1000

    Update .install.sh

--- .install.sh
+++ .install.sh
@@ -1,4 +1,9 @@
 
+# ask for password up-front.
+sudo -v
+# Keep-alive: update existing sudo time stamp if set, otherwise do nothing.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 cd $HOME
 mkdir .dotfiles && cd .dotfiles
 
