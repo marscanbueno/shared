8d261c3d1073160498d06e7efbef3ee414329352
marscanbueno <17656000+marscanbueno@users.noreply.github.com>
2021-06-10 01:01:26 -1000

    Update .install.sh

--- .install.sh
+++ .install.sh
@@ -6,15 +6,20 @@ mkdir .dotfiles && cd .dotfiles
 sudo mv 
 curl -LJO https://raw.githubusercontent.com/marscanbueno/shared/main/hosts
 sudo cp hosts /etc/hosts
-
+sudo chmod o+r /etc/hosts
 
 # motd
 curl -LJO https://raw.githubusercontent.com/marscanbueno/shared/main/motd
 sudo cp motd /etc/motd
-
+sudo chmod o+r /etc/motd
 
 # policybanner.rtfd
 
 mkdir policybanner.rtfd && cd policybanner.rtfd
 curl -LJO https://raw.githubusercontent.com/marscanbueno/shared/main/policybanner.rtfd/TXT.rtf
 curl -LJO https://raw.githubusercontent.com/marscanbueno/shared/main/policybanner.rtfd/Logo.png
+sudo cp policybanner.rtfd /Library/Security/PolicyBanner.rtfd
+sudo chmod -R o+rx /Library/Security/PolicyBanner.rtfd
+
+cd $HOME
+rm -rf .install.sh
