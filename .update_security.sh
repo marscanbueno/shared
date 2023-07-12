f08a52b6e05b2c6f66a43b407922957c07b13a79
marscanbueno <17656000+marscanbueno@users.noreply.github.com>
2023-07-12 11:46:22 -1000

    Update .update_security.sh

--- .update_security.sh
+++ .update_security.sh
@@ -3,51 +3,32 @@ echo ""
 echo ".update_security.sh"
 echo ""
 
-# hosts
-cd $HOME/.dotfiles
-sudo cp hosts /etc/hosts
+# install
+cd $HOME/.dotfiles/shared-main
+sudo mv PolicyBanner.rtfd /Library/Security/
+sudo chmod o+rx /Library/Security/PolicyBanner.rtfd
+sudo rm -rfv PolicyBanner.rtfd
+sudo mv hosts /etc/hosts
 sudo chmod o+r /etc/hosts
-echo ""
-echo "Deleting:"
 sudo rm -rfv hosts
-
-# motd
-cd $HOME/.dotfiles
-sudo cp motd /etc/motd
+sudo mv motd /etc/motd
 sudo chmod o+r /etc/motd
-echo ""
-echo "Deleting:"
 sudo rm -rfv motd
 
-echo "Deleting old PolicyBanner:"
-sudo rm -rfv /Library/Security/PolicyBanner.rtfd
-echo ""
-
-# policybanner.rtfd
-cd $HOME/.dotfiles
-sudo cp -r PolicyBanner.rtfd /Library/Security/PolicyBanner.rtfd
-sudo chmod -R o+rx /Library/Security/PolicyBanner.rtfd
-sudo chmod -R o+r /Library/Security/PolicyBanner.rtfd/NWOPCS.png
-sudo chmod -R o+r /Library/Security/PolicyBanner.rtfd/TXT.rtf
-echo ""
-
-# Update Preboot
-diskutil apfs updatePreboot /
-
-# flush DNS cache
+# flush DNS cache & Update Preboot
 sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder
+diskutil apfs updatePreboot /
 echo ""
 
 # Clean up..
 echo "Deleting:"
-sudo rm -rfv .update_security.sh
 cd $HOME
 sudo rm -rfv .dotfiles
 echo ""
 
 # set volume
 osascript -e "set volume output volume 30"
-say "Security settings downloaded!"
-echo "Security settings downloaded!"
+say "Security settings configured!"
+echo "Security settings configured!"
 
 history -p
