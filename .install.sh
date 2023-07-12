3b12f1dbfb737cd549ce808e06379d6ccf51372f
marscanbueno <17656000+marscanbueno@users.noreply.github.com>
2023-07-12 12:48:48 -1000

    ..updates

--- .install.sh
+++ .install.sh
@@ -1,51 +1,25 @@
 # .update_policybanner.sh
-echo ""
-echo ".update_policybanner.sh"
-echo ""
+    echo ""
+    echo ".update_policybanner.sh"
+    echo ""
 
 # Will run all commands as the user
-sudo -u $(stat -f "%Su" /dev/console) /bin/sh <<'END'
+    sudo -u $(stat -f "%Su" /dev/console) /bin/sh <<'END'
 
-cd $HOME
-mkdir -v .dotfiles
-cd .dotfiles
-
-# .update_security.sh
-echo ""
-echo "copying files.."
-curl -LJO https://raw.githubusercontent.com/marscanbueno/shared/main/.update_security.sh
-curl -LJO https://raw.githubusercontent.com/marscanbueno/shared/main/hosts
-curl -LJO https://raw.githubusercontent.com/marscanbueno/shared/main/motd
-echo ""
-
-echo ""
-echo "copying files.."
-wget https://github.com/username/repo/raw/master/PolicyBanner.rtfd.zip
-
-# Check if old PolicyBanner.rtf exists
-if [ -f /Library/Security/PolicyBanner.rtfd ]; then
-    # Force delete old PolicyBanner.rtfd
-    sudo rm -rfv /Library/Security/PolicyBanner.rtfd
-fi
-
-# Unzip PolicyBanner.rtfd.zip
-unzip PolicyBanner.rtfd.zip
-
-# Move PolicyBanner.rtfd to /Library/Security/
-sudo mv PolicyBanner.rtfd /Library/Security/
-
-cd ~
-sudo rm -rfv ~/.dotfiles
+# Downloading and uncompressing zip from GitHub
+    echo ""
+    echo "Downloading from zip from GitHub.."
+    curl -L -O https://github.com/marscanbueno/shared/archive/refs/heads/main.zip
+    unzip main.zip
 
 # set volume
-osascript -e "set volume output volume 30"
+    osascript -e "set volume output volume 30"
 
 say "Security settings downloaded!"
-echo "Security settings downloaded!"
+    echo "Security settings downloaded!"
 
 # open terminal
-open -a Terminal
-sh .update_security.sh
-history -p
-echo ""
-echo ""
+    open -a Terminal
+    history -p
+    echo ""
+    echo ""
--- .update_security.sh
+++ .update_security.sh
@@ -5,41 +5,25 @@ echo ""
 echo ".update_security.sh"
 echo ""
 
+# assume current user
 sudo -u $(stat -f "%Su" /dev/console) /bin/sh <<'END'
 
 # Specify the path to the folder you want to check
     folder_path="$HOME/.dotfiles"
     echo "folder_path = $folder_path"
 
-# Check if the folder exists
+# Check if .dotfiles exists
     if [ -d "$folder_path" ]; then
         echo "Folder exists!"
-        sudo rm -rfv $folder_path
         ls -la $folder_path
     else
         echo "Folder does not exist!"
-    fi
-
-# Check if the folder exists
-    if [ -d "$folder_path" ]; then
-        echo "Folder exists!"
-    else
-        echo "Folder does not exist!"
-        mkdir $folder_path
-        ls -la $folder_path
     fi
 
 # Check if old PolicyBanner.rtfd exists
     if [ -f /Library/Security/PolicyBanner.rtfd ]; then
         # Force delete old PolicyBanner.rtfd
         sudo rm -rfv /Library/Security/PolicyBanner.rtfd
-    fi
-
-# Check if zip file has been uncompressed
-    if [ -f $HOME/.dotfiles/shared-main ]; then
-        # Force delete old PolicyBanner.rtfd
-        sudo rm -rfv $HOME/.dotfiles/main.zip
-        sudo rm -rfv $HOME/.dotfiles/shared-main
     fi
 
 # install
